c5a464569ea0606b41cc6eff5cd76fae01d04515
Renovato <gabriel.renovato@docato.com.br>
2025-08-25 00:03:09 -0300

    expondo o calendario

--- projects/chrono-calendar/src/public-api.ts
+++ projects/chrono-calendar/src/public-api.ts
@@ -2,7 +2,5 @@
  * Public API Surface of ngx-simple-calendar
  */
 
-export * from './lib/ngx-simple-calendar.service';
-export * from './lib/ngx-simple-calendar.component';
-
+export * from './lib/chrono-calendar/chrono-calendar.component';
 export * from './lib/calendar.model';
